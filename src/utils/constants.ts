40b60e2b820ab039313fb9b1165a76820470c3ce
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-07 15:48:15 -0300

    PA-11631 Generic SBOM API Script

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const CONSTANTS = {
+    SOOS: {
+      API_KEY_ENV_VAR: "SOOS_API_KEY",
+      CLIENT_ID_ENV_VAR: "SOOS_CLIENT_ID",
+    },
+    SBOM: {
+        FILE_REGEX: /\.(cdx|spdx)\.json$/,
+    }
+  };
+  
