2a37fac44a3a9363b8b4abb7c7ba3ac5630bbbe6
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-14 11:31:56 -0300

    remove integration name default

--- src/index.ts
+++ src/index.ts
@@ -92,7 +92,6 @@ class SOOSSBOMAnalysis {
     parser.add_argument("--integrationName", {
       help: "Integration Name - Intended for internal use only.",
       required: false,
-      default: CONSTANTS.SOOS.DEFAULT_INTEGRATION_NAME,
     });
 
     parser.add_argument("--integrationType", {
