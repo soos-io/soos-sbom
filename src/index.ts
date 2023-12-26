1d1e05a6afc01ca8d7ea3b4f9cef180e59b61ba8
SOOS-GSteen <gsteen@soos.io>
2023-12-26 12:06:39 -0500

    c

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,11 @@ class SOOSSBOMAnalysis {
         scanType,
       });
 
-      const exitCodeWithMessage = getAnalysisExitCodeWithMessage(scanStatus, this.args.onFailure);
+      const exitCodeWithMessage = getAnalysisExitCodeWithMessage(
+        scanStatus,
+        this.args.integrationName,
+        this.args.onFailure,
+      );
       soosLogger.always(`${exitCodeWithMessage.message} - exit ${exitCodeWithMessage.exitCode}`);
       exit(exitCodeWithMessage.exitCode);
     } catch (error) {
