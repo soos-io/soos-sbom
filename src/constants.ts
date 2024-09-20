5aab814a4bddb577e6c8c9cdee59fe40df6918bb
Michael Malony <mmalony@soos.io>
2024-09-20 10:16:40 -0400

    PA-15045 Added directoriesToExclude & filesToExclude params

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const SOOS_SBOM_CONSTANTS = {
   FileRegex: /\.(cdx|spdx)\.json$/,
+  FileSyncPattern: "**/*.@(cdx.json|spdx.json)",
   MaxSbomsPerScan: 50,
   UploadBatchSize: 10,
+  DefaultDirectoriesToExclude: ["**/node_modules/**", "**/bin/**", "**/obj/**", "**/lib/**"],
 };
